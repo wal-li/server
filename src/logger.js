65e17fa66c5815479e8d141aedb88b94b0de4c7d
HaoVA <hi@haova.me>
2024-03-17 07:27:53 +0700

    update logger

--- src/logger.js
+++ src/logger.js
@@ -1,33 +1,47 @@
-import pino from 'pino';
-import pretty from 'pino-pretty';
 import colors from 'colors';
-import { LOGGER_LEVELS } from './constants.js';
+
+function getLocalISOString(date) {
+  const offset = date.getTimezoneOffset();
+  const offsetAbs = Math.abs(offset);
+  const isoString = new Date(date.getTime() - offset * 60 * 1000).toISOString();
+  return `${isoString.slice(0, -1)}${offset > 0 ? '-' : '+'}${String(
+    Math.floor(offsetAbs / 60)
+  ).padStart(2, '0')}:${String(offsetAbs % 60).padStart(2, '0')}`;
+}
 
 export const createLogger = (name) => {
-  return pino(
-    {
-      customLevels: LOGGER_LEVELS,
-      useOnlyCustomLevels: true,
-      level: 'http'
-    },
-    pretty({
-      colorize: true,
-      ignore: 'pid,hostname',
-      customPrettifiers: {
-        name(name) {
-          return name;
-        }
-      },
-      messageFormat(log, messageKey) {
-        switch (log.level) {
-          case LOGGER_LEVELS.error:
-            return colors.red(log[messageKey]);
-          case LOGGER_LEVELS.info:
-            return colors.green(log[messageKey]);
-          default:
-            return log[messageKey];
-        }
+  const createLogFunction =
+    (type) =>
+    (...args) => {
+      let logTypeColor;
+
+      switch (type.toLowerCase()) {
+        case 'error':
+          logTypeColor = colors.red;
+          break;
+        case 'info':
+          logTypeColor = colors.green;
+          break;
+        case 'http':
+          logTypeColor = colors.gray;
+          break;
+        default:
+          logTypeColor = colors.gray;
       }
-    })
-  ).child({ name });
+
+      console.log(
+        `[${getLocalISOString(new Date()).split('T')[1]}]`,
+        logTypeColor(type.toUpperCase()),
+        `(${name}):`,
+        ...args.map((msg) => logTypeColor(msg))
+      );
+    };
+
+  return {
+    info: createLogFunction('info'),
+    http: createLogFunction('http'),
+    success: createLogFunction('success'),
+    error: createLogFunction('error'),
+    warn: createLogFunction('warn')
+  };
 };
